c4affeffc44384de0862399bbf28e58224482673
Jeremy Durham <jeremydurham@google.com>
2020-07-30 15:33:35 -0400

    Add signed in status label back in, formatted with swift-format

--- iOS/Bee Plus/Bee Plus/AppDelegate.swift
+++ iOS/Bee Plus/Bee Plus/AppDelegate.swift
@@ -6,34 +6,32 @@
 //  Copyright © 2019 Firebase Dev. All rights reserved.
 //
 
-import UIKit
-import FirebaseCore
 import FirebaseAppDistribution
+import FirebaseCore
 import GoogleSignIn
+import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         setupFirebase()
         setupView()
 
         return true
     }
-    
+
     @available(iOS 9.0, *)
-    func application(_ application: UIApplication, open url: URL, options:[UIApplication.OpenURLOptionsKey : Any]) -> Bool {
-            return GIDSignIn.sharedInstance().handle(url, sourceApplication:options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
-                annotation: [:])
+    func application(_: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any]) -> Bool {
+        return GIDSignIn.sharedInstance().handle(url, sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+                                                 annotation: [:])
     }
 
     func setupFirebase() {
         FirebaseApp.configure()
     }
-    
+
     func setupView() {
         // We've opted not to use storyboards, so instead set up the view and view controller ourselves
         window = UIWindow(frame: UIScreen.main.bounds)
@@ -41,4 +39,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = MainViewController()
     }
 }
-
